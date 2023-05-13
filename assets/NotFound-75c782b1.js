de70237d0c1bbe7bc73e3fa94ad642239cfff150
SzEmil <SzEmil@users.noreply.github.com>
2023-05-13 15:22:48 +0000

    deploy: 9c21f1339c9f09aa30ab208996ec9619ad6a2e6c

--- assets/NotFound-75c782b1.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-ace2ed28.js";const r=()=>o.jsx("div",{children:"NotFound. Check url!"});export{r as default};
---
+++ assets/PokeDex-f4c9111f.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-c6857019.js";const r=()=>e.jsx("div",{children:"PokeDex"});export{r as default};
